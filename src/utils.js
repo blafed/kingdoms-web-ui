9676cd2fdb28c1eb60f984b471b50eb719d7abfb
blafed <a125b25@gmail.com>
2023-09-17 14:42:14 +0300

    migrate to vite and tyypescript

--- src/utils.js
+++
@@ -1,4 +0,0 @@
-export function randomId() {
-  //create random string id from 8 characters
-  return Math.random().toString(36).substr(2, 8)
-}
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
