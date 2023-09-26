ebb44c84c2be1b3f7ca442f0196905d9b34d0088
blafed <a125b25@gmail.com>
2023-09-26 13:22:08 +0300

    preparing to merge

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const apiUrl = "https://localhost:5000/"
-// export const apiUrl = "https://blafed-001-site1.ftempurl.com/"
+// export const apiUrl = "https://localhost:5000/"
+export const apiUrl = "https://blafed-001-site1.ftempurl.com/"
 export const noApi = false
